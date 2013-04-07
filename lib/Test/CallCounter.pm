0df268e71b83dc8bc2b8ca0b368a8be04a55df57
tokuhirom <tokuhirom@gmail.com>
2013-04-07 23:07:14 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 43cf714..48026e0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Test-CallCounter
    
     {{$NEXT}}
    
    +0.04 2013-04-07T14:07:10Z
    +
    +    - Fixed deps.
    +
     0.03 2013-04-04T11:20:12Z
    
         - Restore prototype after wrapping

--- lib/Test/CallCounter.pm
+++ lib/Test/CallCounter.pm
@@ -2,7 +2,7 @@ package Test::CallCounter;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Scalar::Util 1.24 qw(set_prototype);
 use Class::Method::Modifiers qw(install_modifier);
